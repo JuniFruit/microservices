23397ac16b0f636f642f89dfff03366d456f8044
JuniFruit@github.com <idiot228322@mail.ru>
2023-05-15 21:09:52 +0300

    container fixes

--- node/auth/src/app.ts
+++ node/auth/src/app.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import cors from "cors";
-import coookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes";
 import { handleException } from "./middleware/exception.middleware";
 import bodyParser from "body-parser";
@@ -8,8 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(coookieParser());
-// app.use(cors({}));
 app.use("/auth", authRoutes);
 app.get("*", (req, res, next) => {
   res.send("Auth service");
--- node/gateway/src/rabbitmq/connection.ts
+++ node/gateway/src/rabbitmq/connection.ts
@@ -1,12 +1,14 @@
 import amqp, { Channel, Connection } from "amqplib";
 
-const CONNECT_URL = process.env.RABBIT_MQ_URL || "amqp://localhost:5672";
+const CONNECT_URL = process.env.RABBIT_MQ_URL || "";
+
 let channel: Channel | undefined;
 
 let connection: Connection;
 
 async function connectQueue() {
   try {
+    console.log(CONNECT_URL);
     connection = await amqp.connect(CONNECT_URL);
     channel = await connection.createChannel();
     channel.assertQueue(process.env.VIDEO_Q || "", { durable: true });
